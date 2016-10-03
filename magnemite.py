c85187561accccc75541fb2be536265e3b4c96c4
Etsu <etsu4296@gmail.com>
2016-10-03 11:06:51 +0300

    Create magnemite.py

---
+++ magnemite.py
@@ -0,0 +1,21 @@
+import math
+
+f_c = 5200
+f_s = 11700
+delta = 21
+
+F_s = 3 * f_s
+lambda_c = 2 * math.pi * f_c / F_s
+print("Digital Frequency: ", lambda_c)  # цифровая частота
+
+lambda_s = 2 * math.pi * f_s / F_s
+print("Sampling Frequency: ", lambda_s)  # частота выборки
+
+# 20 * math.log10((1 + omega_s ** (2 * n)) ** 1/2  >= 33.5
+omega_c = math.tan(lambda_s/2) / math.tan(lambda_c/2)
+print("Band Edge: ", omega_c)  # граница полосы затухания
+
+# 20 * log10((( 2.914 ^ (2 * n)) ^ 1/2)) >= 21
+
+c = 1 / (math.tan(lambda_c / 2))
+print('C: ', c)
