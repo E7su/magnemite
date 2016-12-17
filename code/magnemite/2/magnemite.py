998676672dd7cb268c3bfd44e39d0275661f9a6f
Etsu <etsu4296@gmail.com>
2016-12-17 21:56:02 +0300

    fix: z

--- code/magnemite/2/magnemite.py
+++ code/magnemite/2/magnemite.py
@@ -99,31 +99,33 @@ for i in range(prec * round(k_period * Fd / F_in)):
     sp_y_in.append(y_in)
 
     if (i % prec) == 0:
-        z02 = z01
-        z01 = temp0
-        temp0 = y_in - b01 * z01 - b02 * z02
-        cascade0 = a00 * temp0 + a01 * z01 + a02 * z02
-
-        z12 = z11
+        z14 = z13
+        z13 = z12
+        z12 = z11 
         z11 = temp1
-        temp1 = cascade0 - b11 * z11 - b12 * z12
-        cascade1 = a10 * temp1 + a11 * z11 + a12 * z12
+        temp1 = y_in - b11 * z11 - b12 * z12 - b13 * z13 - b14 * z14
+        cascade1 = a10 * temp1 + a11 * z11 + a12 * z12 + a13 * z13 + a14 * z14
 
+        z24 = z23
+        z23 = z22
         z22 = z21
         z21 = temp2
-        temp2 = cascade1 - b21 * z21 - b22 * z22
-        cascade2 = a20 * temp2 + a21 * z21 + a22 * z22
+        temp2 = cascade1 - b21 * z21 - b22 * z22 - b23 * z23 - b24 * z24
+        cascade2 = a20 * temp2 + a21 * z21 + a22 * z22 + a23 * z23 + a24 * z24
 
-        
+        z34 = z33
+        z33 = z32
         z32 = z31
         z31 = temp3
-        temp3 = cascade2 - b31 * z31 - b32 * z32
-        cascade3 = a30 * temp3 + a31 * z31 + a32 * z32
+        temp3 = cascade2 - b31 * z31 - b32 * z32 - b33 * z33 - b34 * z34
+        cascade3 = a30 * temp3 + a31 * z31 + a32 * z32 + a33 * z33 + a34 * z34
         
+        z44 = z43
+        z43 = z42
         z42 = z41
         z41 = temp4
-        temp4 = cascade3 - b41 * z41 - b42 * z42
-        cascade4 = a40 * temp4 + a41 * z31 + a42 * z42
+        temp4 = cascade3 - b41 * z41 - b42 * z42 - b43 * z43 - b44 * z44
+        cascade4 = a40 * temp4 + a41 * z31 + a42 * z42 + a43 * z43 + a44 * z44
         
         y_out = cascade4
         sp_y_out.append(y_out)
